f77a66c622c13a09d388baf9c5a2d99242a20ad6
tanimotom <tanimotom@janelia.hhmi.org>
2017-04-24 16:08:07 -0400

    create a simple treasure map

--- apple3.py
+++ apple3.py
@@ -3,3 +3,7 @@ print('A change has been made')
 with open('treasure.txt') as fh:
 	for line in fh:
 		print(line)
+
+with open('map.txt', 'w') as fh:
+  for i in range(10):
+    fh.write("Walk " + str(i) + " paces left.")
